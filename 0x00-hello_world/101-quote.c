34df8edadf8be7cc1343dbf261666b5a843d4313
Eyuel <eyuleo10@gmail.com>
2023-09-07 01:29:18 -0700

    add 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main-entry
+ * return 1
+*/
+
+int main(void)
+{
+	char  *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	fputs(str, stdout);
+	return (1);
+}
