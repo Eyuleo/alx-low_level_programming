d4ac004909bcafcb9dde808b7b4222b111d021c6
Eyuel <eyuleo10@gmail.com>
2023-09-08 01:14:12 -0700

    add 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+**/
+int main(void)
+{
+	int num, combnum;
+
+	for (num = '0' ; num <= '9' ; num++)
+	{
+		for (combnum = num + 1 ; combnum <= '9' ; combnum++)
+		{
+			putchar(num);
+			putchar(combnum);
+			
+			if (num == '8' && combnum == '9')
+			continue;
+
+			putchar(',');
+			putchar(' ');
+			
+		}
+	}
+		putchar('\n');
+	return (0);
+}
