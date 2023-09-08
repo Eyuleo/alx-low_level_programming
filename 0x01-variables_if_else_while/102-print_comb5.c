d5110bd4412c181b745091db1766279bca202dd2
Eyuel <eyuleo10@gmail.com>
2023-09-08 01:41:13 -0700

    add 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+**/
+int main(void)
+{
+	int num, combnum;
+
+	for (num = 0 ; num <= 98 ; num++)
+	{
+		for (combnum = num + 1 ; combnum <= 99 ; combnum++)
+		{
+			putchar((num / 10) + '0');
+			putchar((num % 10) + '0');
+			putchar(' ');
+			putchar((combnum / 10) + '0');
+			putchar((combnum % 10) + '0');
+			
+			if (num == 98 && combnum == 99)
+			continue;
+
+			putchar(',');
+			putchar(' ');
+			
+		}
+	}
+		putchar('\n');
+	return (0);
+}
