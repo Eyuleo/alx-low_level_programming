cecc4559ac35c3271f5400adc59060de2493da43
Eyuel Abiyot <92599318+Eyuleo@users.noreply.github.com>
2023-09-13 14:14:54 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,24 +8,24 @@
 void times_table(void)
 {
 	int i, j;
-	
-    for (i = 0; i < 10; i++)
-    {
-        for(j = 0; j < 10; j++)
-        {
-            int result = i*j;
+	for (i = 0; i < 10; i++)
+    	{
+		for(j = 0; j < 10; j++)
+        	{
+            		int result = i*j;
 
-            if (j == 9)
-	    {
-            printf("%d", result);
-            }
-            else
-            {
-            printf("%d", result);
-            putchar(',');
-            printf(" ");
-            }
-        }
+            		if (j == 9)
+	    		{
+            		printf("%d", result);
+	    		printf(" ");
+            		}
+            		else
+            		{
+            		printf("%d", result);
+            		putchar(',');
+            		printf(" ");
+            		}
+        	}
         printf("\n");
-    }
+    	}
 }
