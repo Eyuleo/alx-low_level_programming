4b48f62995ad9f97bad5127e780422a2f6108530
Eyuel Abiyot <92599318+Eyuleo@users.noreply.github.com>
2023-09-13 14:01:07 +0300

    fix betty errors

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * computes absolute value
- *
- * returns absolute value of integer
-**/
+* _abs - computes the absolute value of an integer
+*
+* @n: takes signed integer
+*
+* Return: Absolute value
+*/
 
 int _abs(int n)
 {
@@ -15,6 +17,7 @@ int _abs(int n)
 	else
 	{
 	int abs = (-1) * n;
+		
 	return (abs);
 	}
 }
