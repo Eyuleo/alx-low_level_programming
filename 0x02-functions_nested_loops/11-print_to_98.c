e600809d76354094d8c122787a4df2cb4439fde1
Eyuel <eyuleo10@gmail.com>
2023-09-13 04:37:01 -0700

    add 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@ void print_to_98(int n)
 {
     if (n < 98)
     {
-    	for (n; n <= 98; n++)
+    	for (; n <= 98; n++)
     	{
         	if (n == 98)
         	{
@@ -25,7 +25,7 @@ void print_to_98(int n)
     }
     else 
     {
-    	for (n; n >= 98; n--)
+    	for (; n >= 98; n--)
     	{
         	if (n == 98)
         	{
