0e302bf81146def0442bf55d32e94106fb29fc29
Eyuel <eyuleo10@gmail.com>
2023-09-13 04:30:45 -0700

    fix errors

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * multiples of 9
+ * times_table - Prints the 9 times table
  *
- * return multiple table
-**/
+ * Return: no return
+ */
 void times_table(void)
 {
 	int i, j, mul;
