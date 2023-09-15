584ac12de716646ae3e8b85dba4cf7a9a8b18004
Eyuel <eyuleo10@gmail.com>
2023-09-15 05:16:46 -0700

    fix betty errors

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,31 +1,33 @@
 #include <stdio.h>
+
 /**
- * main - prints Buzz each numbers of 3 and 5.
+ * main - Entry point, prints "Fizz", "Buzz", or "FizzBuzz" for multiples of 3, 5, or both.
+ *
  * Return: Always 0.
  */
 int main(void)
 {
-	int i;
+    int i;
 
-	for (i = 1; i <= 100; i++)
-	{
-		if ((i % 3 == 0) && ( i % 5 == 0))
-		{
-			printf("FizzBuzz ");
-		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz ");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz ");
-		}
-		else
-		{
-			printf("%d ", i);
-		}
-	}
-	printf("\n");
-	return (0);
+    for (i = 1; i <= 100; i++)
+    {
+        if ((i % 3 == 0) && (i % 5 == 0))
+        {
+            printf("FizzBuzz ");
+        }
+        else if (i % 3 == 0)
+        {
+            printf("Fizz ");
+        }
+        else if (i % 5 == 0)
+        {
+            printf("Buzz ");
+        }
+        else
+        {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+    return (0);
 }
