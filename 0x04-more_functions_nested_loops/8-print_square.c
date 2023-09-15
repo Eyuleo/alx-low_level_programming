c997776b73d880cb119b00d2a861f6591ba35a89
Eyuel <eyuleo10@gmail.com>
2023-09-15 04:43:42 -0700

    add 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_square - prints hashes squares.
+ * @size: size of the square.
+ * Return: no return.
+ */
+void print_square(int size)
+{
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
