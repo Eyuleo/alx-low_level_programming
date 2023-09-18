528729a6a9c9279ba2895a7daa521876bff6ac3b
Eyuel <eyuleo10@gmail.com>
2023-09-18 00:01:04 -0700

    fix betty errors

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 /**
- * void reset_to_98 - set the integer to 98
+ *reset_to_98 - set the integer to 98
  *
- * @n - a pointer we want to set to 98
+ *@n - a pointer we want to set to 98
  *
  * Return - nothing
 */
