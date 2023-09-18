4465fcad6c45a594e277398c1d8e97cdb152c7dd
Eyuel <eyuleo10@gmail.com>
2023-09-18 00:39:10 -0700

    add 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _puts - prints a string followed by new line
+ * @str: - points to a string type
+ *
+ * Return: nothing
+*/
+void _puts(char *str)
+{
+	puts(str);
+	puts('\n');
+}
