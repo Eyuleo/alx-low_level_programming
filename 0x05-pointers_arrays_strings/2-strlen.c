ed7c8d5dc34fc14fa97d74ddd93a3c81fceed354
Eyuel <eyuleo10@gmail.com>
2023-09-18 00:30:21 -0700

    add task-3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+/**
+ * _strlen - returns length of a string
+ * @s: collection of chars or string
+ *
+ * Return: length of strings
+*/
+int _strlen(char *s)
+{
+	int length;
+
+	length = strlen(s);
+	return (length);
+}	
