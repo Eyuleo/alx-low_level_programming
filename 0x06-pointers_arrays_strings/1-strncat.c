f8ae11ae2f0d495b5ad94589596d1ceaf41da380
Eyuel <eyuleo10@gmail.com>
2023-09-20 09:33:50 -0700

    fix errors

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,15 +10,17 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0;
-	int j = 0;
+        int j = 0;
 
 	for (; dest[i] != '\0'; i++)
 	{
 	}
-	for (; src[j] != '\0'; j++)
+
+	for (; src[j] != '\0' && j < n; j++)
 	{
 		dest[i + j] = src[j];
 	}
+
 	dest[i + j] = '\0';
-	return (dest);
+	return dest;
 }
