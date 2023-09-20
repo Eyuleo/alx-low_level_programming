ed74cfc9c7df519026d9c94263a05ed6e7ef1160
Eyuel <eyuleo10@gmail.com>
2023-09-20 02:13:32 -0700

    add task-1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+/**
+ * _strncat - concatnate two strings
+ * @dest: destination string
+ * @src: source string
+ * @n: max number of bytes to concatnate from src
+ *
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j = 0;
+
+	for (; dest[i] != '\0'; i++)
+	{
+	}
+
+	for (; src[j] != '\0' && j < n; j++)
+	{
+		dest[i + j] = src[j];
+	}
+
+	dest[i + j] = '\0';
+	return dest;
+}
