569e21bf1fd5f5d2ee1ef040148f8afa5f5e35f6
Eyuel <eyuleo10@gmail.com>
2023-09-20 09:27:42 -0700

    fix betty errors

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,17 +10,14 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0;
-        int j = 0;
+	int j = 0;
 
 	for (; dest[i] != '\0'; i++)
-	{
-	}
 
-	for (; src[j] != '\0' && j < n; j++)
+	for (; src[j] != '\0'; j++)
 	{
 		dest[i + j] = src[j];
 	}
-
 	dest[i + j] = '\0';
-	return dest;
+	return (dest);
 }
