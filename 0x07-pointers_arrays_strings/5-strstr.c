84049ebec6325859dbcdc3b8a4a542460fef578c
Eyuel <eyuleo10@gmail.com>
2023-09-25 04:01:25 -0700

    fix task-5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,20 +8,19 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
+	int i = 0, j;
 
-	for (i = 0; haystack[i] != '\0'; i++)
+	while (haystack[i] != '\0')
 	{
-		if (haystack[i] == needle[0])
-		{
-			for (j = 0; needle[j] != '\0'; j++)
-			{
-				if (haystack[i + j] != needle[j])
-					break;
-			}
-			if (needle[j] == '\0')
-				return (&haystack[i]);
-		}
+		j = 0;
+
+		while (haystack[i + j] == needle[j] && needle[j] != '\0')
+			j++;
+
+		if (needle[j] == '\0')
+			return (haystack + i);
+	i++;
 	}
-	return (NULL);
+
+	return (0);
 }
