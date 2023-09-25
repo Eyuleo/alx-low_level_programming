1aa0421d696fb9b08efd43b929b414c1b5aa6e07
Eyuel <eyuleo10@gmail.com>
2023-09-24 23:40:03 -0700

    add 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset -  fills memory with const byte
+ * @s: pointer to the memory area to fill
+ * @b: the constant byte
+ * @n: the numbers of byte to fill
+ *
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+<h1 align="center">Even more pointers, arrays and strings</h1>
