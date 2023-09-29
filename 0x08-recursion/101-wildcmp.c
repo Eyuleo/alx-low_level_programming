113456c2643845c8e9e66f54d1cbbd2354d92d61
Eyuel <eyuleo10@gmail.com>
2023-09-29 04:46:59 -0700

    add task-8

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * wildcmp - return 1 if s1 is idnetical to s2 0 if not
+ * @s1: the string to be compared
+ * @s2: string that is being compared with s1
+ *
+ * Return: 1 0r 0.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s2 == '*')
+	{
+		return (1);
+	}
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 2));
+	}
+	return (0);
+}
