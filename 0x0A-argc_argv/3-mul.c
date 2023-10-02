a01f01b0757714ac7b584c7bc64f551007eaf2ed
Eyuel <eyuleo10@gmail.com>
2023-10-02 04:47:36 -0700

    add task-3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 or 1
+ */
+int main(int argc, char *argv[])
+{
+	int i = 1, result = 1;
+
+	if (argc >= 2)
+	{
+		for (; i < argc; i++)
+		{
+			int value;
+
+			value = atoi(argv[i]);
+			result *= value;
+		}
+		printf("%d\n", result);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
