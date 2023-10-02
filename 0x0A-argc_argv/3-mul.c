2dcf3260f865a6bb8126096f756cbcbd0b0ee4a1
Eyuel <eyuleo10@gmail.com>
2023-10-02 09:53:32 -0700

    update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,22 +9,16 @@
  */
 int main(int argc, char *argv[])
 {
-	int i = 1, result = 1;
-	int value;
+	int num1, num2, result;
 
-	if (argc >= 2)
-	{
-		for (; i < argc; i++)
-		{
-			value = atoi(argv[i]);
-			result *= value;
-		}
-		printf("%d\n", result);
-	}
-	else
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+	printf("%d\n", result);
 	return (0);
 }
