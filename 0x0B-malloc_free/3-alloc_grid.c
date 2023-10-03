e443322f9f96700c7cc8eabbf9c81f26c1cf3fe4
Eyuel <eyuleo10@gmail.com>
2023-10-03 04:20:25 -0700

    fix testing errors

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,7 +24,7 @@ int **alloc_grid(int width, int height)
 		grid[i] = malloc(width * sizeof(int));
 		if (grid[i] == NULL)
 		{
-			for (j = 0; j < i; i++)
+			for (j = 0; j < i; j++)
 			{
 				free(grid[j]);
 			}
