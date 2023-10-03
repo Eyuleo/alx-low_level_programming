f213c9d63025703515d956d2cbe9e2ef797440e9
Eyuel <eyuleo10@gmail.com>
2023-10-03 02:14:29 -0700

    fix errors

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,6 +11,10 @@ char *str_concat(char *s1, char *s2)
 	int len1, len2, i = 0;
 	char *new_str;
 
+	if (s1 == NULL && s2 == NULL)
+	{
+		return (NULL);
+	}
 	len1 = (s1 != NULL) ? strlen(s1) : 0;
 	len2 = (s2 != NULL) ? strlen(s2) : 0;
 	new_str = malloc(((len1 + len2) + 1) * sizeof(char));
@@ -21,18 +25,12 @@ char *str_concat(char *s1, char *s2)
 	}
 	if (s1 != NULL)
 	{
-		for (; s1[i] != '\0'; i++)
-		{
-			new_str[i] = s1[i];
-		}
+		strcpy(new_str, s1);
+		i += len1;
 	}
 	if (s2 != NULL)
 	{
-		for (; s2[i] != '\0'; i++)
-		{
-			new_str[i + len1] = s2[i];
-		}
+		strcpy(new_str + i, s2);
 	}
-	new_str[i] = '\0';
 	return (new_str);
 }
