47bf76e475c22b5eefd7d49bb567e3574778b0c2
Eyuel <eyuleo10@gmail.com>
2023-10-09 05:00:28 -0700

    fix placement of the structs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,7 +22,7 @@ void print_dog(struct dog *d);
 /**
  * dog_t - new name for the type struct dog
  */
-typedef struct dog_t dog;
+typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 #endif
