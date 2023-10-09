c498718649b59e7652cd4792035b41db509c121f
Eyuel <eyuleo10@gmail.com>
2023-10-09 03:47:03 -0700

    initial commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * struct dog - type of dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: defines a struct type of dog with the above elements
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
