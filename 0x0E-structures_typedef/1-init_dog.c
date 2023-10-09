d53a69bf4d6215919c39f5fb31ebe7a5b4518dd9
Eyuel <eyuleo10@gmail.com>
2023-10-09 04:19:21 -0700

    fix errors

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 #include "dog.h"
 /**
  * init_dog - initialize a variable of type struct dog
@@ -12,12 +11,11 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
-	{
-		strncpy(d->name, name, sizeof(d->name));
-		d->name[sizeof(d->name) - 1] = '\0';
-		d->age = age;
-		strncpy(d->owner, owner, sizeof(d->owner));
-		d->owner[sizeof(d->owner) - 1] = '\0';
-	}
+	d = malloc(sizeof(struct dog));
+
+	if (d == NULL)
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
