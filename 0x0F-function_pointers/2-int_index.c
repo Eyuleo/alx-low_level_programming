5757b01d81f2818594af425e57ab954821ef133e
Eyuel <eyuleo10@gmail.com>
2023-10-11 02:28:05 -0700

    fix errors

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,17 +9,14 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i = 0, result;
+	int i = 0;
 
-	if (array == NULL || cmp == NULL || size <= 0)
+	if (size <= 0)
 		return (-1);
 	for (; i < size; i++)
 	{
-		result = cmp(array[i]);
-		if (result == 0)
-		{
+		if (cmp(array[i]) != 0)
 			return (i);
-		}
 	}
 	return (-1);
 }
