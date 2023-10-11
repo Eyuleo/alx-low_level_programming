786dff642083a010c7fea321d96f826eec710f68
Eyuel <eyuleo10@gmail.com>
2023-10-11 02:24:16 -0700

    fix code fails

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,12 +11,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0, result;
 
-	if (size <= 0)
+	if (array == NULL || cmp == NULL || size <= 0)
 		return (-1);
 	for (; i < size; i++)
 	{
 		result = cmp(array[i]);
-		if (result > 0)
+		if (result == 0)
 		{
 			return (i);
 		}
