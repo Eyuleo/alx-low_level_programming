0238031cd4be0d174f4f961f3ded9844d539723a
Eyuel <eyuleo10@gmail.com>
2023-10-11 02:09:42 -0700

    add advanced task-4

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints op codes
+ * @argc: arg count
+ * @argv: arg pointer
+ * 
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int num_bytes, i = 0;
+	char *ptr = (char*) &main;
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	num_bytes = atoi(argv[1]);
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (; i < num_bytes; i++)
+		printf("%02x ", (unsigned char)ptr[i]);
+	printf("\n");
+}
