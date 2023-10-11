d4cb5f49869c1c0910ab500a8b88b74226862946
Eyuel <eyuleo10@gmail.com>
2023-10-11 02:34:48 -0700

    add conditional cases

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,6 +9,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i = 0;
 
+	if (array == NULL || action == NULL)
+		return;
 	for (; i < size; i++)
 		action(array[i]);
 }
