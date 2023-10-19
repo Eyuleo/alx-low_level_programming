225c0f8361dee5fb2a86978c006ee0c8ba1e0291
Eyuel <eyuleo10@gmail.com>
2023-10-19 10:25:03 -0700

    fix typo variablr

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -28,7 +28,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	else
 	{
 		current = *head;
-		while (curren->next != NULL)
+		while (current->next != NULL)
 			current = current->next;
 		current->next = new_node;
 	}
