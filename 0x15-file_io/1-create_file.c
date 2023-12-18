ba1741d86f060190b1f03e755c9547341c937c67
Eyuleo <eyuleo10@gmail.com>
2023-12-18 03:41:15 -0800

    fix a bunch of errors

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,33 +1,38 @@
 #include "main.h"
+
 /**
- * create_file - function that creates a file.
- * @filename: variable pointer
- * @text_content: content file
- * Description: Create a function that creates a file.
- * Return: 1 on success, -1 on failure
- */
+* create_file - creates a file
+* @filename: the filename to create
+* @text_content: a NULL terminated string to write to the file
+* Return: 1 (Success),  -1 (failure)
+*/
+
 int create_file(const char *filename, char *text_content)
 {
-	int i = 0, file;
+	int fd;
+	int len, res;
 
-	if (filename == NULL)
+	if (!filename)
+		return (-1);
+	/* open */
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (fd == -1)
 		return (-1);
 
-	if (text_content == NULL)
-		text_content = "";
+	if (!text_content)
+		return (1);
+	len = 0;
+	while (text_content[len] != '\0')
+		len++;
 
-
-	while (text_content[i] != '\0')
-	{
-		i++;
-	}
-
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	if (file == -1)
+	res = write(fd, text_content, len);
+	if (res != len)
 		return (-1);
-
-	write(file, text_content, i);
+	/*close*/
+	res = close(fd);
+	if (res == -1)
+		exit(-1);
 
 	return (1);
 }
