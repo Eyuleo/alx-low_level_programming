c3d5a29fe01d7767a6d03c5676aa1f57bcc81da9
Eyuleo <eyuleo10@gmail.com>
2023-12-18 03:42:27 -0800

    fix checker req errors

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,33 +1,38 @@
 #include "main.h"
+
 /**
- * append_text_to_file - that appends text at the end of a file
- * @filename: variable pointer
- * @text_content: content file
- * Description: function that appends text at the end of a file
- * Return: 1 on success, -1 on failure
- */
+* append_text_to_file - appens text at the end of a file
+* @filename: the file to append to
+* @text_content: the text that will be appended to the file
+* Return: 1 (Success), -1 (Failure)
+*/
+
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int i = 0, file;
+	int fd, len, res;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-
-	if (text_content == NULL)
-		text_content = "";
-
-
-	while (text_content[i] != '\0')
+	/*open the file*/
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (text_content)
 	{
-		i++;
+		len = 0;
+		while (text_content[len])
+			len++;
+		res = write(fd, text_content, len);
+		if (res == -1)
+			return (-1);
+
+		res = close(fd);
+		if (res != 0)
+			exit(-1);
+		return (1);
 	}
-
-	file = open(filename, O_WRONLY | O_APPEND);
-
-	if (file == -1)
-		return (-1);
-
-	write(file, text_content, i);
-
+	res = close(fd);
+	if (res != 0)
+		exit(-1);
 	return (1);
 }
